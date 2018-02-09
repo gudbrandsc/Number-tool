19aedd50aa4051b7bcd569ec9d813a843be28a45
gudbrandsc <gudbrand.schistad@gmail.com>
2018-02-09 21:14:42 +0000

    Finished all number convertions and made it so when a number is typed the right output get's printed to the console

--- number_converter.c
+++ number_converter.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 /*Flips all bits in a bit array and returns it*/
 char* bit_flip(char *bits){
-
   char *flipBits = (char *)malloc(strlen(bits)+1);
   size_t bit_length = strlen(bits);
   strcpy(flipBits,bits);
@@ -20,18 +19,20 @@ char* bit_flip(char *bits){
   }
   return flipBits;
 }
+/*Method to get the bitvalue from a signed or unsigned integer*/
 char *int_to_bits(int n) {
   int nbits = sizeof(n) * 8;
   char *s = malloc(nbits+1);  // +1 for '\0' terminator
   s[nbits] = '\0';
   // forcing evaluation as an unsigned value prevents complications
   // with negative numbers at the left-most bit
+
   unsigned int u = *(unsigned int*)&n;
   int i;
   unsigned int mask = 1 << (nbits-1); // fill in values right-to-left
-  for (i = 0; i < nbits; i++, mask >>= 1)
+  for (i = 0; i < nbits; i++, mask >>= 1){
     s[i] = ((u & mask) != 0) + '0';
-  printf("Full:: %s\n",s );
+  }
   return s;
 }
 
@@ -59,6 +60,19 @@ unsigned bits_to_unsigned(char *bits){
     val = bits_to_int(bits);
   }
   return val;
+}
+
+/*Prints a whitespace for each 4th bit*/
+void format_and_print_bits(char* bits){
+  int count = 0;
+  for(int i = 0; i <= strlen(bits);i++){
+    if(count%4==0 && count != 0){
+      printf(" ");
+    }
+    printf("%c",bits[i]);
+    count += 1;
+  }
+  printf(" (Base 2)\n");
 }
 
 /*Return a unsigned int value of the bit*/
@@ -134,6 +148,20 @@ int pos_int_to_unsigned(char* value) {
   }
   return intvalue;
 }
+unsigned int negativ_int_to_unsigned(char* value){
+  unsigned int unsigned_val;
+  int int_value = 0;
+  int multi = 1;
+  int curr_val;
+
+  for(int i = strlen(value)-1; i>=1; i--){
+    int_value+= ((value[i]-'0')*multi);
+    multi= (multi*10);
+  }
+  int_value = int_value * (-1);
+  unsigned_val = (unsigned) int_value;
+  return unsigned_val;
+}
 
 int check_if_negativ_int(int value_size, char* value) {
   for(int i = 1; i < value_size; i++){
@@ -177,6 +205,7 @@ unsigned int convert_value(int type, char* value){
 
   if(type==1){
     printf("%s\n","its a negatv" );
+    return negativ_int_to_unsigned (value);
   }else if(type==2){
     return hex_to_unsigned(value);
   }else if(type==3){
@@ -194,23 +223,45 @@ unsigned int verify_argv_value(char* value){
 
   unsigned int unsigned_value = 0;
 
-  if(value[0]=='-' && isdigit(value[1])){
+  if(value[0]=='-' && isdigit(value[1])){//If value is negativ
     if(check_if_negativ_int(value_size, value) == 0){
       unsigned_value = convert_value(1, value);
+      printf("%d\n",unsigned_value);
+      format_and_print_bits(int_to_bits(unsigned_value));
+      printf("0b%s (base 2)\n",int_to_bits(unsigned_value));
+      printf("0x%X (base 16)\n",unsigned_value);
+      printf("%u (base 10 unsigned)\n",unsigned_value);
+      printf("%d (base 10 signed)\n",unsigned_value);
       return unsigned_value;
     }
-  }else if(value[0]=='0' && value[1]=='x' && value_size > 2){
+  }else if(value[0]=='0' && value[1]=='x' && value_size > 2){//If value is hexadecimal
     if(check_if_hexadecimal(value_size, value) == 0){
       unsigned_value = convert_value(2, value);
+      format_and_print_bits(int_to_bits(unsigned_value));
+      printf("0b%s (base 2)\n",int_to_bits(unsigned_value));
+      printf("0x%X (base 16)\n",unsigned_value);
+      printf("%u (base 10 unsigned)\n",unsigned_value);
+      printf("%d (base 10 signed)\n",unsigned_value);
+
       return unsigned_value;
     }
-  }else if(value[0]=='0' && value[1]=='b' && value_size > 2){
+  }else if(value[0]=='0' && value[1]=='b' && value_size > 2){// If value is a binary
     if(check_if_binary(value_size, value) == 0){
       unsigned_value = convert_value(3, value);
+      format_and_print_bits(int_to_bits(unsigned_value));
+      printf("0b%s (base 2)\n",int_to_bits(unsigned_value));
+      printf("0x%X (base 16)\n",unsigned_value);
+      printf("%u (base 10 unsigned)\n",unsigned_value);
+      printf("%d (base 10 signed)\n",unsigned_value);
       return unsigned_value;
     }
-  }else if(check_if_int(value_size, value)==0){
+  }else if(check_if_int(value_size, value)==0){ //If value is a possetiv integer
     unsigned_value = convert_value(4, value);
+    format_and_print_bits(int_to_bits(unsigned_value));
+    printf("0b%s (base 2)\n",int_to_bits(unsigned_value));
+    printf("0x%X (base 16)\n",unsigned_value);
+    printf("%u (base 10 unsigned)\n",unsigned_value);
+    printf("%d (base 10 signed)\n",unsigned_value);
     return unsigned_value;
   }
   return 0;
@@ -223,10 +274,11 @@ int main(int argc, char* argv[]) {
     printf("invalid input\n");
     }
   */
+
   unsigned int value = verify_argv_value(argv[1]);
-  printf("%d\n",value );
-  char* bin = int_to_bits(-10);
-  printf("Full:: %s\n",bin );
+
+
+
 
   return 0;
 }
